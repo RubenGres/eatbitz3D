2758102b2d3e492cd861ef55bd797918e56041d1
RubenGres <ruben.gres@proton.me>
2026-03-09 14:58:04 +0100

    better highlight

--- godot/Scenes/3D_scene/PointCloudObject/highlight_sprite.gd
+++ godot/Scenes/3D_scene/PointCloudObject/highlight_sprite.gd
@@ -1,5 +1,10 @@
 @tool
 extends Sprite3D
 
+func _ready():
+	self.material_override = self.material_override.duplicate()
+
 func _on_point_cloud_from_bitz_rembg_texture_loaded(texture) -> void:
 	self.material_override.set_shader_parameter("sprite_texture", texture)
+	self.texture = texture
+	
